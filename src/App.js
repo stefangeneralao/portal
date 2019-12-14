7329554ae4627e0082ab91bf307510acc9be31ee
Stefan Generalao <stefan.generalao@gmail.com>
2019-12-14 11:03:10 +0800

    Intial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>stefangeneralao.com</h1>
       </header>
+      <nav>
+        <a href="http://stefangeneralao.com/portfolio">
+          Portfolio
+        </a>
+        <a href="http://stefangeneralao.com/exterior-interior">
+          Exterior Interior Classifier
+        </a>
+        <a href="http://stefangeneralao.com/skrt-ai">
+          Skrt AI
+        </a>
+        <a href="http://stefangeneralao.com/skrt-human">
+          Skrt Human
+        </a>
+        <a href="http://stefangeneralao.com/genetic-path-finder">
+          Genetic Path Finder
+        </a>
+      </nav>
+      <footer>
+
+      </footer>
     </div>
   );
 }
